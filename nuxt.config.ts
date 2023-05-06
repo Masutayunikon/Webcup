55b77c8d4bde9a225403e8c54f1731033a24a5a6
Jojolions <emilie.baunifais@epitech.eu>
2023-05-06 16:39:01 +0400

    navbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
     // @ts-ignore
     css: ['@/assets/styles.css'],
     modules: ['nuxt-icon', '@nuxtjs/tailwindcss' ],
+    tailwindcss: {
+        exposeConfig: true,
+        // exposeLevel: 1,  // determines tree-shaking (optional)
+    },
     build: {
         transpile: [ 'troisjs', 'gsap']
     }
