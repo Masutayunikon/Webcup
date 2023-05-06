3f96ac4c9966c0b0962a84c3b5c84ea927267144
yahaya <sandati.yahaya@epietch.eu>
2023-05-06 21:03:01 +0400

    contact forms

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="mobile-menu-button" tabindex="-1">
+      <a href="/contact" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="mobile-menu-item-0">Contact</a>
+    </div>
     <card-3D class="card"
              image="https://i.pinimg.com/564x/f3/91/28/f391287b9b76dd596117ff7383d895cb.jpg"
              title="I'm a title"
@@ -37,5 +40,6 @@
       aspect-ratio: 10/16;
       border-radius: 25px;
     }
+    ;
   }
 </style>
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+      extend: {
+        colors: {
+          'royal-blue': '#145870',
+          'golden-yellow': '#D48745',
+          'jade-green': '#8C9C72'
+        }
+      }
+    }
+}
